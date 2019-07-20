5fb02855002aefbb4150c4eb6259a2dd521150c9
Nuntuch <nuntuch13589@gmail.com>
2019-07-20 12:22:32 +0700

    ใส่ iframe ลงหน้า tools

---
+++ iframe_script-menutool.js
@@ -0,0 +1,28 @@
+
+// jQuery(document).ready(function($){
+
+// All your code using $
+
+// });
+
+
+$(document).ready(function(){
+// jQuery(document).ready(function($){
+  //the trigger on hover when cursor directed to this class
+    $(".core-menu li").hover(
+    function(){
+      //i used the parent ul to show submenu
+        $(this).children('ul').slideDown('fast');
+    }, 
+      //when the cursor away 
+    function () {
+        $('ul', this).slideUp('fast');
+    });
+  //this feature only show on 600px device width
+    $(".hamburger-menu").click(function(){
+      $(".burger-1, .burger-2, .burger-3").toggleClass("open");
+        $(".core-menu").slideToggle("fast");
+    });
+});
+/** credit:@rafonzoo 
+http://rafonzo.blogspot.co.id/ **/
